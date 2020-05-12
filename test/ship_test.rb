28070e7cd2c04bcb5483de6d6a24cb952c6c95be
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-12 14:49:25 -0600

    ship_test passing instance and attributes test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,7 +7,7 @@ class ShipTest < Minitest::Test
 def test_ship_exists_and_holds_attributes
   cruiser = Ship.new("Cruiser", 3)
   assert_equal "Cruiser", cruiser.name
-  assert_equal 3, cruiser.name
+  assert_equal 3, cruiser.length
   assert_equal 3, cruiser.health
 end
 
