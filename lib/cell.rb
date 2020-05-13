c4cfbd10a6fd3bed7ecc6f5a1c51aa63945c31ea
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-12 21:06:33 -0600

    Pass cell fired upon? test

--- lib/cell.rb
+++ lib/cell.rb
@@ -13,10 +13,31 @@ class Cell
 
   def place_ship(ship_obj)
     @ship = ship_obj
+    @fired_upon = false
   end
 
-  def render
-    "."
+  def fire_upon
+
   end
+
+
+  def render(reveal=false)
+    if reveal == true && !empty?
+      "S"
+    elsif reveal == true && @ship == nil
+      "M"
+    else
+      "."
+    end
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+# ”.” if the cell has not been fired upon.
+# “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss).
+# “H” if the cell has been fired upon and it contains a ship (the shot was a hit).
+# “X” if the cell has been fired upon and its ship has been sunk.
+
 
 end
