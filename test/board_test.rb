522cdffa067948b13404ce2e2e708139f1329062
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-13 17:59:40 -0600

    tested board instance, data type, length, and coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,7 +9,9 @@ class BoardTest < MiniTest::Test
   def test_it_exists_with_attributes
     board = Board.new
     assert_instance_of Board, board
-    assert_equal ({}), board.cells
+    assert_instance_of Hash, board.cells
+    assert_equal 16, board.cells.length
+    assert_equal "D3", board.cells["D3"].coordinate
   end
 
 end
