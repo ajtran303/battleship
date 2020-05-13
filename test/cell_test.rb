d0bd0212cfefcc730abe8679834212fcfdbb7784
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-12 23:19:42 -0600

    Add test for cell render H

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -30,34 +30,37 @@ class CellTest < MiniTest::Test
 
   def test_it_renders_a_cell_with_dot
     cell_1 = Cell.new("B4")
-    # cell_2 = Cell.new("C3")
-    # cruiser = Ship.new("Cruiser", 3)
-
     assert_equal ".", cell_1.render
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    assert_equal ".", cell_2.render
+
   end
 
   def test_it_renders_to_M_when_fired_upon_and_is_empty
     cell_1 = Cell.new("B4")
 
-    cell_1.render
-
+    assert_equal ".", cell_1.render
     assert_equal true, cell_1.empty?
 
     cell_1.fire_upon
+
     assert_equal "M", cell_1.render
+    assert_equal true, cell_1.fired_upon?
   end
 
-  def test_it_can_render_to_s_when_fired_upon_a_ship
+  def test_it_can_change_render_from_dot_to_S_when_it_has_ship
     cell_2 = Cell.new("C3")
-
     cruiser = Ship.new("Cruiser", 3)
 
     cell_2.place_ship(cruiser)
-
     assert_equal false, cell_2.empty?
 
     assert_equal ".", cell_2.render
-
     assert_equal "S", cell_2.render(true)
   end
 
@@ -75,6 +78,8 @@ class CellTest < MiniTest::Test
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
 
+    assert_equal false, cell.fired_upon?
+
     cell.fire_upon
 
     assert_equal true, cell.fired_upon?
@@ -86,6 +91,7 @@ class CellTest < MiniTest::Test
     cell.place_ship(cruiser)
 
     assert_equal 3, cell.ship.health
+    assert_equal false, cell.fired_upon?
 
     cell.fire_upon
 
@@ -93,6 +99,38 @@ class CellTest < MiniTest::Test
     assert_equal true, cell.fired_upon?
   end
 
-end
+  def test_it_does_not_change_M_when_render_true
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+    assert_equal true, cell_1.empty?
 
-  # test_it_renders_to_H_when_fired_upon_and_has_a_ship
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+    assert_equal "M", cell_1.render(true)
+    assert_equal "M", cell_1.render
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    assert_equal false, cell_2.empty?
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+  end
+
+  def test_it_renders_to_H_when_fired_upon_and_has_a_ship
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    assert_equal "S", cell_2.render(true)
+    assert_equal ".", cell_2.render
+
+    cell_2.fire_upon
+
+    assert_equal false, cell_2.empty?
+    assert_equal "H", cell_2.render
+    assert_equal "H", cell_2.render(true)
+  end
+
+end
