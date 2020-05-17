fb252f06fb511f3cb27db6415cda3896a6d40e46
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-16 18:19:42 -0600

    Add refactored cell render tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -35,4 +35,43 @@ class CellTest < MiniTest::Test
     assert_equal true, @cell.fired_upon?
   end
 
+  # render tests
+  def test_it_renders_dot_if_not_fired_upon
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+  end
+
+  def test_it_renders_M_if_the_shot_was_a_miss
+    cell_1 = Cell.new("B4")
+    cell_1.fire_upon
+
+    assert_equal "M", cell_1.render
+  end
+
+  def test_it_renders_a_ship_as_dot_or_S_if_render_true
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+  end
+
+  def test_it_renders_H_if_the_shot_hit_or_X_if_ship_sunk
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+
+    assert_equal "H", cell_2.render
+    assert_equal false, cruiser.sunk?
+
+    2.times { cruiser.hit }
+    assert_equal true, cruiser.sunk?
+
+    assert_equal "X", cell_2.render
+  end
+
 end
