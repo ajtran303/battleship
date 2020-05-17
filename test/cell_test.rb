b29da739b3c954fae38e0309d61ff404311443e4
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-16 18:06:01 -0600

    Add tests for cell knows it is fired upon

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -16,7 +16,7 @@ class CellTest < MiniTest::Test
 
   def test_it_has_attributes
     assert_equal "B4", @cell.coordinate
-    assert_equal nil, @cell.ship
+    assert_nil @cell.ship
   end
 
   def test_empty?
@@ -27,6 +27,12 @@ class CellTest < MiniTest::Test
     @cell.place_ship(@cruiser)
     assert_equal @cruiser, @cell.ship
     assert_equal false, @cell.empty?
+  end
+
+  def test_it_knows_if_it_is_fired_upon
+    assert_equal false, @cell.fired_upon?
+    @cell.fire_upon
+    assert_equal true, @cell.fired_upon?
   end
 
 end
