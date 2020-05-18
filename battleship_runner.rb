9699c503633484ef513296203acccd5dcccf1a0a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-18 00:31:38 -0600

    Add runner file to invoke starter class

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,10 +1,3 @@
-require "./lib/board"
-require "./lib/cell"
-require "./lib/ship"
-require "./lib/setup_computer"
-require "./lib/setup"
-
-setup = Setup.new
-
-setup.place_cpu_ships
-setup.place_player_ships
+require "./lib/starter"
+starter = Starter.new
+starter
