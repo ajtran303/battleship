d57dab303e508c718f53bdabdb8537dea5a7aa46
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-17 23:05:39 -0600

    Pass cpu coord attr tests and add methods for CLI output

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,8 +2,11 @@ require "./lib/setup"
 
 class Turn
 
+  attr_reader :player_coord
+
   def initialize
-    @setup = Setup.new
+    setup = Setup.new
+    @setup = setup
     @player_coord = nil
     @cpu_coord = nil
   end
@@ -18,43 +21,52 @@ class Turn
     puts @setup.player_board.render(true)
   end
 
+  def update_player_coord(valid_coord)
+    @player_coord = valid_coord
+  end
+
+  def fire_at_cpu
+    if @setup.cpu_board.cells["#{@player_coord}"].fired_upon?
+      puts ["Again? Bold strategy!",
+            "The ships do not move in this game!",
+            "It was not very effective...",
+            "You threw away your shot!!!",
+            "You already shot there!!!"].sample
+      puts "Hint: Next time shoot somewhere else!"
+      end
+    @setup.cpu_board.cells["#{@player_coord}"].fire_upon
+  end
+
   def player_fire
-    puts "Enter the coordinate for your shot:"
-      #gets.chomp
-      #valid_coordinate?(gets.chomp)
-      #if true fire upon coordinate
-      #else "Please enter a valid coordinate:"
-      #if coord already fired upon inform player
-      #reassign @player_coord
+    loop do
+      puts "Enter the coordinate for your shot:"
+      player_shot_coord = gets.chomp.upcase
+      if @setup.player_board.valid_coordinate?(player_shot_coord)
+        update_player_coord(player_shot_coord)
+        break
+      else
+        puts "Those are invalid coordinates. Please try again:"
+      end
+    end
+    fire_at_cpu
   end
 
   def cpu_fire
-    #???
-    #fired_upon_array?
-    #pick random coord
-    #if not fired upon, fire upon
-    #reassign @cpu_coord
   end
 
   def report_player_results
-    #Your shot on A4 was a miss.
+    x = get_player_shot_result
+    results = { "M" => "was a miss",
+                "H" => "hit a ship",
+                "X" => "sunk a ship" }
+    puts "Your shot on #{@player_coord} #{results[x]}."
+  end
+
+  def get_player_shot_result
+    @setup.cpu_board.cells["#{@player_coord}"].render
   end
 
   def report_cpu_results
-    # My shot on C1 was a miss.
   end
 
 end
-
-turn = Turn.new
-
-turn.render_cpu
-turn.render_player
-turn.player_fire
-turn.cpu_fire
-turn.report_player_results
-turn.report_cpu_results
-#check game_over?
-# A shot missed
-# A shot hit a ship
-# A shot sunk a ship
